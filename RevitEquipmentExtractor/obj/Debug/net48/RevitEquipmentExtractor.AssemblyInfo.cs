71375476717eb8901755abd076135a860d787c2f
mhouston-motzengineering <mhouston@motzengineering.com>
2025-04-21 15:07:56 -0400

    Fixing issues with pathing

--- RevitEquipmentExtractor/obj/Debug/net48/RevitEquipmentExtractor.AssemblyInfo.cs
+++ RevitEquipmentExtractor/obj/Debug/net48/RevitEquipmentExtractor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RevitEquipmentExtractor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e122658b003800a808235614ea6417dcdfc72fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ae15eced4c2ea3972ac7984dee2c0fa4133862e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RevitEquipmentExtractor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RevitEquipmentExtractor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
